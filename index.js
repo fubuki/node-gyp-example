a67848818ef6edd869c4898fd9b009c8f36bd366
fubuki <fubuki@users.noreply.github.com>
2014-05-05 23:09:32 +0800

    js 型別 跟 V8型別之間的判斷和轉換。

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ var assert = require('assert');
 var hello = require('./build/Release/hello');
 assert.equal('world', hello.hello());
 console.log('hello.hello() =', hello.hello());
+
+
+console.log('hello.detect(123) =', hello.detect(123));
+
+console.log('hello.detect([]) =', hello.detect([]));
--- test_hello.cc
+++ test_hello.cc
@@ -4,12 +4,35 @@
 using namespace v8;
 
 Handle<Value> Method(const Arguments& args) {
-  HandleScope scope;
-  return scope.Close(String::New("world"));
+  	HandleScope scope;
+  	return scope.Close(String::New("world"));
 }
 
+Handle<Value> detect(const Arguments& args) {
+  	HandleScope scope;
+  	Local<Value> arg = args[0];
+  	bool isArray = arg->IsArray();
+	bool isBoolean = arg->IsBoolean();
+	bool isNumber = arg->IsNumber();
+	bool isInt32 = arg->IsInt32();
+
+	if (isNumber) {
+
+		return scope.Close(String::New("isNumber"));
+	} else if (isArray) {
+		return scope.Close(String::New("isArray"));
+	} else {
+		return scope.Close(String::New("the other thing"));
+	}
+
+
+  	
+}
+
+
 void init(Handle<Object> target) {
-  NODE_SET_METHOD(target, "hello", Method);
+	NODE_SET_METHOD(target, "hello", Method);
+	NODE_SET_METHOD(target, "detect", detect);
 }
 
 NODE_MODULE(hello, init);
